a57686e966a48d085027ae4ba32b4e536b30295d
107590006 <t107590006@ntut.org.tw>
2021-03-08 14:53:16 +0800

    add list set unit test

--- src/main/java/org/ntutssl/termfrequency/StopWordManager.java
+++ src/main/java/org/ntutssl/termfrequency/StopWordManager.java
@@ -2,15 +2,19 @@ package org.ntutssl.termfrequency;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class StopWordManager{
-    private List<String> stopWords;
+    private List<String> stopWordsList;
+    private Set<String> stopWordsSet;
 
     public StopWordManager(){
-        this.stopWords = new ArrayList<>();
+        this.stopWordsList = new ArrayList<>();
+        this.stopWordsSet = new HashSet<>();
         try(Scanner sc = new Scanner(new File("input/stop_words.txt"))){
             sc.useDelimiter(",");
             while(sc.hasNext()){
--- src/test/java/org/ntutssl/termfrequency/StopWordManagerTest.java
+++ src/test/java/org/ntutssl/termfrequency/StopWordManagerTest.java
@@ -6,14 +6,33 @@ import org.junit.Test;
 
 public class StopWordManagerTest{
     @Test
-    public void test_stop_word_the(){
+    public void test_list_is_stop_word_first(){
         StopWordManager swm = new StopWordManager();
-        assertTrue(swm.isStopWord("the"));
+        assertTrue(swm.isStopWordList("a"));
     }
-    
-    public void test_is_stop_word_first_and_last(){
+
+    public void test_list_is_stop_word_middle(){
         StopWordManager swm = new StopWordManager();
-        assertTrue(swm.isStopWord("a"));
-        assertTrue(swm.isStopWord("your"));
+        assertTrue(swm.isStopWordList("me"));
+    }
+
+    public void test_list_is_stop_word_last(){
+        StopWordManager swm = new StopWordManager();
+        assertTrue(swm.isStopWordList("your"));
+    }
+
+    public void test_set_is_stop_word_first(){
+        StopWordManager swm = new StopWordManager();
+        assertTrue(swm.isStopWordSet("a"));
+    }
+
+    public void test_set_is_stop_word_middle(){
+        StopWordManager swm = new StopWordManager();
+        assertTrue(swm.isStopWordSet("me"));
+    }
+
+    public void test_set_is_stop_word_last(){
+        StopWordManager swm = new StopWordManager();
+        assertTrue(swm.isStopWordSet("your"));
     }
  }
