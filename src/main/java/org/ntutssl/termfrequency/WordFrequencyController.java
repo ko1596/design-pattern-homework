e1e18e8048ac4e350ab97253fc378a8a87d79c1d
107590006 <t107590006@ntut.org.tw>
2021-03-09 00:11:39 +0800

    add main funtion

--- src/main/java/org/ntutssl/termfrequency/WordFrequencyController.java
+++ src/main/java/org/ntutssl/termfrequency/WordFrequencyController.java
@@ -1,5 +1,6 @@
 package org.ntutssl.termfrequency;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -8,5 +9,33 @@ public class WordFrequencyController{
         StopWordManager swm = new StopWordManager(args[0]);
         DataStorageManager dsm = new DataStorageManager(args[1]);
         WordFrequencyManager wfm = new WordFrequencyManager();
+        Integer range = Integer.parseInt(args[2]);
+        String order = args[3];
+        LinkedHashMap<String, Integer> orderWord=null;
+
+        for (String word : dsm.getWords()){
+            if(!swm.isStopWordList(word)) wfm.incrementCount(word);
+        }
+        
+        switch (order) {
+            case "asc":
+                orderWord = new LinkedHashMap<>(wfm.getWordFrequencyAscending());
+                break;
+            
+            case "des":
+                orderWord = new LinkedHashMap<>(wfm.getWordFrequencyDescending());
+                break;
+
+            default:
+                break;
+        }
+        int count=0;
+        for (Map.Entry<String, Integer> ent : orderWord.entrySet()){
+            if(count<range){
+                System.out.println(ent.getKey() + ": " + ent.getValue());
+                count++;
+            }
+            else break;
+        }
      }
 }
