cbfc9754f4bc8259e4f71fe1c69809723c0fabc5
107590006 <t107590006@ntut.org.tw>
2021-03-15 02:57:00 +0800

    fix err out of range

--- src/main/java/org/ntutssl/termfrequency/WordFrequencyManager.java
+++ src/main/java/org/ntutssl/termfrequency/WordFrequencyManager.java
@@ -59,6 +59,9 @@ public class WordFrequencyManager implements IWordFrequencyManager {
      }
 
     public void output(String outputPath, String order, int range, IOHandler handler) {
+        if(range<1||range>getNumOfWords()){
+            throw new WordFrequencyException("Out of range! The range should be from 1 to "+getNumOfWords()+".");
+        }
         if(order.equals("asc") ){
             handler.handleOutput(outputPath, range, getWordFrequency(SortOrder.ASCENDING));
         }
