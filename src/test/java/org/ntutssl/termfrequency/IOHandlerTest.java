d18e0e1bb76359eb4adedbf4877d2f49a784e1ed
107590006 <t107590006@ntut.org.tw>
2021-03-14 18:12:20 +0800

    add ioheadler handleInputAsSet unit test

--- src/test/java/org/ntutssl/termfrequency/IOHandlerTest.java
+++ src/test/java/org/ntutssl/termfrequency/IOHandlerTest.java
@@ -49,4 +49,48 @@ public class IOHandlerTest {
         assertTrue(test_list_string.contains("ebooks"));
     }
 
+    //--------------------------------------------------------------------------------------------------------
+
+    @Test
+    public void test_set_input_stop_word_first_word(){
+        IOHandler ioh = new IOHandler();
+        List<String> test_set_string = ioh.handleInputAsSet("input/stop_words.txt", ",");
+        assertTrue(test_set_string.contains("a"));
+    }
+
+    @Test
+    public void test_set_input_stop_word_middle_word(){
+        IOHandler ioh = new IOHandler();
+        List<String> test_set_string = ioh.handleInputAsSet("input/stop_words.txt", ",");
+        assertTrue(test_set_string.contains("let"));
+    }
+
+    @Test
+    public void test_set_input_stop_word_last_word(){
+        IOHandler ioh = new IOHandler();
+        List<String> test_set_string = ioh.handleInputAsSet("input/stop_words.txt", ",");
+        assertTrue(test_set_string.contains("your"));
+    }
+
+    @Test
+    public void test_set_input_Content_first_word(){
+        IOHandler ioh = new IOHandler();
+        List<String> test_set_string = ioh.handleInputAsSet("input/pride-and-prejudice.txt", "[\\W_]+");
+        assertTrue(test_set_string.contains("the"));
+    }
+
+    @Test
+    public void test_set_input_Content_middle_word(){
+        IOHandler ioh = new IOHandler();
+        List<String> test_set_string = ioh.handleInputAsSet("input/pride-and-prejudice.txt", "[\\W_]+");
+        assertTrue(test_set_string.contains("light"));
+    }
+
+    @Test
+    public void test_set_input_Content_last_word(){
+        IOHandler ioh = new IOHandler();
+        List<String> test_set_string = ioh.handleInputAsSet("input/pride-and-prejudice.txt", "[\\W_]+");
+        assertTrue(test_set_string.contains("ebooks"));
+    }
+
  }
