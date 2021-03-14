480d6e626071022f5913eae2726f71e94853c7dc
107590006 <t107590006@ntut.org.tw>
2021-03-15 00:48:18 +0800

    add WFM unit test and function

--- src/main/java/org/ntutssl/termfrequency/WordFrequencyManager.java
+++ src/main/java/org/ntutssl/termfrequency/WordFrequencyManager.java
@@ -1,20 +1,70 @@
 package org.ntutssl.termfrequency;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.SortOrder;
+
 public class WordFrequencyManager implements IWordFrequencyManager {
-    public WordFrequencyManager() { }
+    private Map<String, Integer> words;
+    
+    public WordFrequencyManager() {
+        this.words = new Hashtable<>();
+    }
 
-    public void incrementCount(String word) { }
+    public void incrementCount(String word) {
+        if(this.words.containsKey(word)){
+            this.words.put(word,this.words.get(word) + 1);
+        }else{
+            this.words.put(word, 1);
+        }
+     }
 
-    public int getNumOfWords() { }
+    public int getNumOfWords() {
+        return this.words.size();
+     }
 
-    public List<String> getWordFrequency(SortOrder order) { }
 
-    private List<String> sort(SortOrder order) { }
+     public List<String> getWordFrequency(SortOrder order) {
+         List<Map.Entry<String, Integer>> list = new ArrayList<>(this.words.entrySet());
+         List<String> outputList = new ArrayList<>();
+ 
+         if(order==SortOrder.ASCENDING){
+             Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
+                 @Override
+                 public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2){
+                     return o1.getValue().compareTo(o2.getValue());
+                 }
+             });
+         }
+         else if(order==SortOrder.DESCENDING){
+             Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
+                 @Override
+                 public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2){
+                     return o2.getValue().compareTo(o1.getValue());
+                 }
+             });
+         }
+ 
+         for(Map.Entry<String, Integer> map: list){
+             outputList.add(map.getKey() + ": " + map.getValue() + "\n");
+         }
+ 
+         return outputList;
+ 
+     }
 
-    public void output(
-        String outputPath,
-        String order,
-        int range,
-        IOHandler handler
-    ){ }
+    public void output(String outputPath, String order, int range, IOHandler handler) {
+        if(order.equals("asc") ){
+            handler.handleOutput(outputPath, range, getWordFrequency(SortOrder.ASCENDING));
+        }
+        else if(order.equals("des")){
+            handler.handleOutput(outputPath, range, getWordFrequency(SortOrder.DESCENDING));
+        }
+    }
+
 }
--- src/test/java/org/ntutssl/termfrequency/WordFrequencyManagerTest.java
+++ src/test/java/org/ntutssl/termfrequency/WordFrequencyManagerTest.java
@@ -1,3 +1,48 @@
 package org.ntutssl.termfrequency;
 
-public class WordFrequencyManagerTest { }
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class WordFrequencyManagerTest {
+    @Test
+    public void test_get_number_of_word(){
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        wfm.incrementCount("dog");
+        wfm.incrementCount("dog");
+        wfm.incrementCount("cat");
+        wfm.incrementCount("cat");
+        assertEquals(2, wfm.getNumOfWords());
+    }
+
+    @Test
+
+    public void test_WFMS_output_is_work(){
+        IOHandler ioh = new IOHandler();
+        List<String> read_data = new ArrayList<>();
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        wfm.incrementCount("dog");
+        wfm.incrementCount("dog");
+        wfm.incrementCount("cat");
+        wfm.incrementCount("cat");
+        wfm.incrementCount("cat");
+        wfm.output("output/output.txt", "des", 2, ioh);
+
+        try(Scanner sc = new Scanner(new File("output/output.txt"))){
+            sc.useDelimiter("\n");
+            while(sc.hasNext()){
+                read_data.add(sc.next().toLowerCase());
+            }
+        }catch(IOException e){
+            System.out.println("Cannot found the file.");
+        }
+        assertTrue(read_data.contains("dog: 2"));
+    }
+ }
