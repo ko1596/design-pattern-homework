077bcea741fae7349cb02d0d09856b793fd76b76
107590006 <t107590006@ntut.org.tw>
2021-03-15 13:56:02 +0800

    completed main

--- src/main/java/org/ntutssl/termfrequency/Main.java
+++ src/main/java/org/ntutssl/termfrequency/Main.java
@@ -3,8 +3,8 @@ package org.ntutssl.termfrequency;
 public class Main {
     public static void main(String[] args){
         IOHandler ioHandler = new IOHandler();
-        IStopWordManager swm = new StopWordManager();
-        IDataStorageManager dsm = new DataStorageManager();
+        IStopWordManager swm = new StopWordManager(args[0],ioHandler);
+        IDataStorageManager dsm = new DataStorageManager(args[1],ioHandler);
         IWordFrequencyManager wfm = new WordFrequencyManagerStream();
         new WordFrequencyController(ioHandler, swm, dsm, swm).run(args[4], Integer.parseInt(args[3]) , args[2]);;
      }
