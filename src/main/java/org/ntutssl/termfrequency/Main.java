e20a6ef3d5432fbb83a2127ec41a362d8c8d4b5a
107590006 <t107590006@ntut.org.tw>
2021-03-15 13:33:31 +0800

    add main and WFC

--- src/main/java/org/ntutssl/termfrequency/Main.java
+++ src/main/java/org/ntutssl/termfrequency/Main.java
@@ -1,5 +1,11 @@
 package org.ntutssl.termfrequency;
 
 public class Main {
-    public static void main(String[] args){ }
+    public static void main(String[] args){
+        IOHandler ioHandler = new IOHandler();
+        IStopWordManager swm = new StopWordManager();
+        IDataStorageManager dsm = new DataStorageManager();
+        IWordFrequencyManager wfm = new WordFrequencyManagerStream();
+        new WordFrequencyController(ioHandler, swm, dsm, swm).run(args[4], Integer.parseInt(args[3]) , args[2]);;
+     }
 }
