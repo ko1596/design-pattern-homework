d34f0ba066fc0d1e99ffb0a980f1a17c09854345
107590006 <t107590006@ntut.org.tw>
2021-03-15 22:41:21 +0800

    add WFM unit test

--- src/test/java/org/ntutssl/termfrequency/WordFrequencyManagerTest.java
+++ src/test/java/org/ntutssl/termfrequency/WordFrequencyManagerTest.java
@@ -1,55 +1,112 @@
 package org.ntutssl.termfrequency;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
+import javax.swing.SortOrder;
 
 import org.junit.Test;
 
 public class WordFrequencyManagerTest {
-    @Test
-    public void test_get_number_of_word(){
+    @Test(expected = WordFrequencyException.class)
+    public void return_exception_if_order_is_invalid(){
+        IOHandler ioh = new IOHandler();
         WordFrequencyManager wfm = new WordFrequencyManager();
-        wfm.incrementCount("dog");
-        wfm.incrementCount("dog");
-        wfm.incrementCount("cat");
-        wfm.incrementCount("cat");
-        assertEquals(2, wfm.getNumOfWords());
+        wfm.incrementCount("word");
+        wfm.incrementCount("map");
+        wfm.output("output/output2.txt", "invalid", 2, ioh);
+    }
+    @Test(expected = WordFrequencyException.class)
+    public void return_exception_if_range_exceeds_lower_limit(){
+        IOHandler ioh = new IOHandler();
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        wfm.incrementCount("word");
+        wfm.incrementCount("map");
+        wfm.output("output/output2.txt", "des", -1, ioh);
+    }
+    @Test(expected = WordFrequencyException.class)
+    public void return_exception_if_range_exceeds_upper_limit(){
+        IOHandler ioh = new IOHandler();
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        wfm.incrementCount("word");
+        wfm.incrementCount("map");
+        wfm.output("output/output2.txt", "des", 3, ioh);
+    }
+    @Test(expected = WordFrequencyException.class)
+    public void return_exception_if_word_not_found(){
+        IOHandler ioh = new IOHandler();
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        wfm.output("output/output2.txt", "des", 1, ioh);
     }
 
     @Test
-
-    public void test_WFMS_output_is_work(){
-        IOHandler ioh = new IOHandler();
-        List<String> read_data = new ArrayList<>();
+    public void test_wfm_output_is_work(){
         WordFrequencyManager wfm = new WordFrequencyManager();
-        wfm.incrementCount("dog");
-        wfm.incrementCount("dog");
-        wfm.incrementCount("cat");
-        wfm.incrementCount("cat");
-        wfm.incrementCount("cat");
-        wfm.incrementCount("pig");
-        wfm.incrementCount("pig");
-        wfm.incrementCount("pig");
-        wfm.incrementCount("pig");
-        wfm.incrementCount("pig");
-        wfm.incrementCount("pig");
-        wfm.incrementCount("pig");
-        wfm.output("output/output.txt", "asc", 3, ioh);
-
-        try(Scanner sc = new Scanner(new File("output/output.txt"))){
-            sc.useDelimiter("\n");
-            while(sc.hasNext()){
-                read_data.add(sc.next().toLowerCase());
-            }
-        }catch(IOException e){
-            System.out.println("Cannot found the file.");
-        }
-        assertTrue(read_data.contains("dog: 2"));
+        List<String> testList = new ArrayList<>();
+        wfm.incrementCount("word");
+        testList.add("word: 1\n");
+        assertEquals(testList, wfm.getWordFrequency(SortOrder.DESCENDING));
+    }
+    @Test
+    public void test_count_a_word_twice_should_return_two(){
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        List<String> testList = new ArrayList<>();
+        wfm.incrementCount("word");
+        wfm.incrementCount("word");
+        testList.add("word: 2\n");
+        assertEquals(testList, wfm.getWordFrequency(SortOrder.DESCENDING));
+    }
+    @Test
+    public void test_count_two_words_should_return_correct_map(){
+        List<String> testList = new ArrayList<>();
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        wfm.incrementCount("map");
+        wfm.incrementCount("word");
+        testList.add("map: 1\n");
+        testList.add("word: 1\n");
+        assertEquals(testList, wfm.getWordFrequency(SortOrder.DESCENDING));
+    }
+    @Test
+    public void test_empty_array_in_the_beginning(){
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        assertEquals(0, wfm.getNumOfWords());
+    }
+    @Test
+    public void test_get_num_of_words(){
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        wfm.incrementCount("word");
+        assertEquals(1, wfm.getNumOfWords());
+    }
+    @Test
+    public void test_sort_ascending(){
+        List<String> testList = new ArrayList<>();
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        wfm.incrementCount("map");
+        wfm.incrementCount("word");
+        wfm.incrementCount("word");
+        wfm.incrementCount("set");
+        wfm.incrementCount("set");
+        wfm.incrementCount("set");
+        testList.add("map: 1\n");
+        testList.add("word: 2\n");
+        testList.add("set: 3\n");
+        assertEquals(testList, wfm.getWordFrequency(SortOrder.ASCENDING));
+    }
+    @Test
+    public void test_sort_descending(){
+        List<String> testList = new ArrayList<>();
+        WordFrequencyManager wfm = new WordFrequencyManager();
+        wfm.incrementCount("map");
+        wfm.incrementCount("word");
+        wfm.incrementCount("word");
+        wfm.incrementCount("set");
+        wfm.incrementCount("set");
+        wfm.incrementCount("set");
+        testList.add("set: 3\n");
+        testList.add("word: 2\n");
+        testList.add("map: 1\n");
+        assertEquals(testList, wfm.getWordFrequency(SortOrder.DESCENDING));
     }
  }
