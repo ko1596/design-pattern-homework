870fd535b0fd1d1aa111779d6b0897838c4fa81c
107590006 <t107590006@ntut.org.tw>
2021-03-22 21:07:01 +0800

    completed Document interface

--- src/main/java/org/ntutssl/document/Document.java
+++ src/main/java/org/ntutssl/document/Document.java
@@ -1,3 +1,17 @@
 package org.ntutssl.document;
 
-public interface Document {  }
+public interface Document {
+    default Document getContent(int index){
+        throw new DocumentException("Invalid action: getContent");
+    };
+
+    String getText();
+
+    default int getLevel(){
+        throw new DocumentException("Invalid action: getLevel");
+    };
+  
+    default void add(Document document){
+        throw new DocumentException("Invalid action: add");
+    };
+  }
