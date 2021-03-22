3e740216eac0d5585964e9e49146a67cce37f6eb
107590006 <t107590006@ntut.org.tw>
2021-03-22 21:26:04 +0800

    add Paragraph unit test

--- src/test/java/org/ntutssl/document/ParagraphTest.java
+++ src/test/java/org/ntutssl/document/ParagraphTest.java
@@ -1,5 +1,27 @@
 package org.ntutssl.document;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class ParagraphTest {  }
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class ParagraphTest {
+    @Test
+    public void test_for_content(){
+        Document title = new Paragraph("abc");
+        assertEquals("abc", title.getText());
+    }
+
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
+
+    @Test
+    public void test_for_err(){
+        Document title = new Paragraph("abc");
+        expected.expect(DocumentException.class);
+        expected.expectMessage("Invalid action: add");
+        title.add(title);
+
+    }
+}
