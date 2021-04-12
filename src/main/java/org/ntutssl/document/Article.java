e3c55e72f0c0c8daa575dc7a45111d1746952eac
107590006 <t107590006@ntut.org.tw>
2021-04-12 21:03:04 +0800

    Articel, Editor, Doucument interface; completed

--- src/main/java/org/ntutssl/document/Article.java
+++ src/main/java/org/ntutssl/document/Article.java
@@ -1,37 +1,49 @@
 package org.ntutssl.document;
 
-public class Article implements Document {
-  
-  public Article(String topic, int level) {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class Article implements Document {
+  private String topic;
+  private List<Document> allDocumenList = new ArrayList<>();
+  private int level;
+  public Article(String topic, int level) {
+    this.topic = topic;
+    this.level = level;
   }
 
   public String getText() {
-
+    return topic;
   }
 
   @Override
-  public int getLevel() {
-    
-  }
+  public int getLevel() { 
+    return level;
+   }
   
   @Override
   public void add(Document document) {
+    if(document.getClass() == Article.class){
+      if(document.getLevel() < this.level){
+        throw new DocumentException("Invalid action: add an article with lower level into article");
+      }else this.allDocumenList.add(document);
+    }else this.allDocumenList.add(document);
     
   }
 
   @Override
   public Iterator<Document> iterator() {
-    
+    return this.allDocumenList.iterator();
   }
 
   @Override
   public void accept(Visitor visitor) {
-    
+    visitor.visitArticle(this);
   }
 
   @Override
   public String toString() {
-    
+    return "Article\t\t" + "topic: " + this.getText() + "\n\t\t" + "level: " + this.getLevel() + "\n";
   }
 }
--- src/main/java/org/ntutssl/document/Editor.java
+++ src/main/java/org/ntutssl/document/Editor.java
@@ -1,19 +1,26 @@
 package org.ntutssl.document;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Editor {  
+
+  private List<Document> allEditor;
+
   public Editor() {
-    
+    this.allEditor = new ArrayList<>();
   }
 
   public void add(Document document) {
-    
+    this.editor.add(document);
   }
 
   public int size() {
-    
+    return this.allEditor.size();
   }
 
   public Iterator<Document> iterator() {
-    
+    return this.allEditor.iterator();
   }
 }
