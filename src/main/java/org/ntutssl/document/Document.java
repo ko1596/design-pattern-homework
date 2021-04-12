46275b46d760bf6bb172a1efb5b737b883dc9e0f
107590006 <t107590006@ntut.org.tw>
2021-04-12 20:52:47 +0800

    DoucumentException, Completed

--- src/main/java/org/ntutssl/document/Document.java
+++ src/main/java/org/ntutssl/document/Document.java
@@ -1,20 +1,22 @@
 package org.ntutssl.document;
 
+import java.util.Iterator;
+
 public interface Document {  
   public default int getLevel() {
-    
+    throw new DocumentException("Invalid action: getLevel");
   }
   
   public default void add(Document document) {
-    
+    throw new DocumentException("Invalid action: add");
   }
   
   public default Iterator<Document> iterator() {
-    
+    throw new DocumentException("Invalid action: Iterator is null");
   }
   
   public default int getSize() {
-    
+    throw new DocumentException("Invalid action: getSize");
   }
 
   public String getText();
--- src/main/java/org/ntutssl/document/DocumentException.java
+++ src/main/java/org/ntutssl/document/DocumentException.java
@@ -3,5 +3,7 @@ package org.ntutssl.document;
 public class DocumentException extends RuntimeException {
   static final long serialVersionUID = 1L;
 
-  public DocumentException(String errorMessage) { }
+  public DocumentException(String errorMessage) {
+    super(errorMessage);
+   }
 }
