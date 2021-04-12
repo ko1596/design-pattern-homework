0e2cdf72095c7e8ad2eb7735c17fcc00fc66244f
107590006 <t107590006@ntut.org.tw>
2021-04-12 21:08:48 +0800

    Title; completed

--- src/main/java/org/ntutssl/document/Title.java
+++ src/main/java/org/ntutssl/document/Title.java
@@ -1,25 +1,31 @@
 package org.ntutssl.document;
 
 public class Title implements Document {
+
+  private String text;
+  private Integer size;
+
   public Title(String text, int size) {
-    
+    this.text = text;
+    this.size = size;
   }
 
   public String getText() {
-    
+    return this.text;
   }
 
+  @Override
   public int getSize() {
-    
+    return this.getSize();
   }
 
   @Override
   public void accept(Visitor visitor) {
-    
+    visitor.visitTitle(this);
   }
 
   @Override
   public String toString() {
-    
+    return "Title\t\t" + "text: " + this.getText() + "\n\t\t" + "size: " + this.getSize() + "\n";
   }
 }
