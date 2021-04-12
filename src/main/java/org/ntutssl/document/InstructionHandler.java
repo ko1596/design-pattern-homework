c47a82adbb950682a020a1541e4dd6aef712339c
107590006 <t107590006@ntut.org.tw>
2021-04-13 00:11:56 +0800

    main and run; completed

--- src/main/java/org/ntutssl/document/InstructionHandler.java
+++ src/main/java/org/ntutssl/document/InstructionHandler.java
@@ -1,8 +1,6 @@
 package org.ntutssl.document;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 public class InstructionHandler {
@@ -97,21 +95,58 @@ public class InstructionHandler {
 		System.out.print("Level of article: ");
 		int level = input.nextInt();
 		input.nextLine();
+		if(level < 0 || level < lastLevel) {
+			System.out.println("Invalid Input: The level should be positive or higher than the level of the current article");
+			return null;
+		}
+		Article article = new Article(topic, level);
+		while(true){
+			printArticleInstructions();
+			String instruction = input.nextLine();
+			if(!instruction.equals("exit")) handleArticleInstructions(instruction, article);
+			else break;
+		}
+		return article;
 	}
 
 	private void printArticleInstructions() {
-		
+		System.out.println("Please enter the following instruction to edit the article: ");
+		System.out.println(" 1. 'add title': to add a title to the article");
+		System.out.println(" 2. 'add paragraph': to add a paragraph to the article");
+		System.out.println(" 3. 'add article': to add an article to the article");
+		System.out.println(" 4. 'exit': to exit the process");
 	}
 
 	private boolean handleArticleInstructions(String instruction, Article article) {
-		
+		if(instruction.equals("add article")) {
+			article.add(addArticleInstruction(article.getLevel()));
+			System.out.println("Article added to the article.");
+		}else if(instruction.equals("add paragraph")) {
+			article.add(addParagraphInstruction());
+			System.out.println("Paragraph added to the article.");
+		}else if(instruction.equals("add title")) {
+			article.add(addTitleInstruction());
+			System.out.println("Title added to the article.");
+		}else{
+			System.out.println("Invalid instruction");
+			return true;
+		}
+		return false;
 	}
 
 	private void findContentInstruction() {
-		
+		System.out.print("Enter the word you want to find: ");
+		String target = input.nextLine();
+		FindContentVisitor fcv = new FindContentVisitor(target);
+		Iterator<Document> it = this.editor.iterator();
+		while(it.hasNext()) it.next().accept(fcv);
+		for (Document i : fcv.getResult()) System.out.println(i.toString());
 	}
 
 	private void outputHtmlInstruction() {
-    
+		HtmlOutputVisitor hov = new HtmlOutputVisitor();
+		Iterator<Document> it = this.editor.iterator();
+		while(it.hasNext()) it.next().accept(hov);
+		System.out.println(hov.getResult());
 	}
 }
