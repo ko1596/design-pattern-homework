d3f7373eceac0118d82b332dc592b22f474d362a
107590006 <t107590006@ntut.org.tw>
2021-04-12 21:40:53 +0800

    FCV; completed

--- src/main/java/org/ntutssl/document/FindContentVisitor.java
+++ src/main/java/org/ntutssl/document/FindContentVisitor.java
@@ -1,8 +1,17 @@
 package org.ntutssl.document;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class FindContentVisitor {  
+
+  private List<Document> documents;
+  private String target;
+
   public FindContentVisitor(String target) {
-    
+    this.documents = new ArrayList<>();
+    this.target = target.toLowerCase();
   }
 
   public void visitParagraph(Paragraph paragraph) {
@@ -18,6 +27,6 @@ public class FindContentVisitor {
   }
   
   public List<Document> getResult() {
-    
+    return documents;
   }
 }
