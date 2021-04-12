915b087782a7b7bccf3eb79517aadc7516b31620
107590006 <t107590006@ntut.org.tw>
2021-04-12 21:14:24 +0800

    null iterator; completed

--- src/main/java/org/ntutssl/document/NullIterator.java
+++ src/main/java/org/ntutssl/document/NullIterator.java
@@ -3,14 +3,14 @@ package org.ntutssl.document;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class NullIterator {
+public class NullIterator implements Iterator<Document>{
   @Override
   public boolean hasNext() {
-    
+    return false;
   }
 
   @Override
   public Document next() {
-    
+    throw new NoSuchElementException("Do not have element.");
   }
 }
