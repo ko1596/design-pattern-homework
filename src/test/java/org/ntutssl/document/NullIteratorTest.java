641393bd6fc15079572a699fad4717c76c8ba37d
107590006 <t107590006@ntut.org.tw>
2021-04-13 17:46:41 +0800

    NullIteratorTest, compeleted

--- src/test/java/org/ntutssl/document/NullIteratorTest.java
+++ src/test/java/org/ntutssl/document/NullIteratorTest.java
@@ -1,5 +1,28 @@
 package org.ntutssl.document;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Iterator;
+
 import org.junit.Test;
 
-public class NullIteratorTest { }
+public class NullIteratorTest {
+    @Test
+    public void test_has_next(){
+        Editor editor = new Editor();
+        Document article = new Article("topic", 3);
+        editor.add(article);
+        Iterator<Document> it = editor.iterator();
+        assertTrue(it.hasNext());
+    }
+
+    @Test
+    public void test_next_element(){
+        Editor editor = new Editor();
+        Document article = new Article("topic", 3);
+        editor.add(article);
+        Iterator<Document> it = editor.iterator();
+        assertEquals(article, it.next());
+    }
+ }
