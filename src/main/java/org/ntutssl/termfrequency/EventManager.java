dcf7bc1a54f77cdbb4f67d2ebabc5c160e0286cb
107590006 <t107590006@ntut.org.tw>
2021-05-06 20:03:55 +0800

    eventManager, completed

--- src/main/java/org/ntutssl/termfrequency/EventManager.java
+++ src/main/java/org/ntutssl/termfrequency/EventManager.java
@@ -5,15 +5,21 @@ import java.util.Map;
 
 public class EventManager {
 
-  private Map<EventType, EventListener> event;
+  private Map<EventType, EventListener> eventMap;
 
   public EventManager() { 
-    event = new HashMap<>();
+    eventMap = new HashMap<>();
   }
 
   public void subscribe(EventType eventType, EventListener listener) { 
-    event.put(eventType, listener);
+    eventMap.put(eventType, listener);
   }
 
-  public void publish(EventType eventType, String event) { }
+  public void publish(EventType eventType, String event) {
+    for(Map.Entry<EventType, EventListener> i:eventMap.entrySet()){
+      if(i.getKey()==eventType){
+        i.getValue().onEvent(eventType, event);
+      }
+    }
+   }
 }
