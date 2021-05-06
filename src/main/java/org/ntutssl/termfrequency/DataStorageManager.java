9f618242699a7d42366709c74ac2a6ce2892592c
107590006 <t107590006@ntut.org.tw>
2021-05-06 20:27:23 +0800

    Event Manager test, add

--- src/main/java/org/ntutssl/termfrequency/DataStorageManager.java
+++ src/main/java/org/ntutssl/termfrequency/DataStorageManager.java
@@ -1,13 +1,18 @@
 package org.ntutssl.termfrequency;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataStorageManager {
 
-  public DataStorageManager(EventManager eventManager) { }
+  private List<String> words;
+
+  public DataStorageManager(EventManager eventManager) {
+    words = new ArrayList<>();
+   }
 
   public List<String> getWords() {
-    return null;
+    return words;
    }
 
   public void onEvent(EventType eventType, String event) { }
--- src/test/java/org/ntutssl/termfrequency/EventManagerTest.java
+++ src/test/java/org/ntutssl/termfrequency/EventManagerTest.java
@@ -1,3 +1,22 @@
 package org.ntutssl.termfrequency;
 
-public class EventManagerTest { }
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class EventManagerTest implements EventListener{
+    public int startcount =0;
+    public void onEvent(EventType eventType, String event){
+        if(eventType == EventType.START) startcount++;
+    }
+    
+    @Test
+    public void test_START(){
+        EventManager em = new EventManager();
+        em.subscribe(EventType.START, this);
+        em.publish(EventType.START, "");
+        assertEquals(1, startcount);
+        em.publish(EventType.START, "");
+        assertEquals(2, startcount);
+    }
+ }
