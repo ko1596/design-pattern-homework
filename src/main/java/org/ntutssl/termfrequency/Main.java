45fe707ca9e863872895f9ba2cc699e7c0ae9405
107590006 <t107590006@ntut.org.tw>
2021-05-08 19:38:22 +0800

    main, completed

--- src/main/java/org/ntutssl/termfrequency/Main.java
+++ src/main/java/org/ntutssl/termfrequency/Main.java
@@ -2,5 +2,18 @@ package org.ntutssl.termfrequency;
 
 public class Main {
   
-  public static void main(String[] args) { }
+  public static void main(String[] args) {
+    String dataPath= args[0];
+    int range= Integer.parseInt(args[1]);
+    String sortOrder= args[2];
+
+    EventManager eventManager = new EventManager();
+    DataStorageManager dsm = new DataStorageManager(eventManager);
+    StopWordManager swm = new StopWordManager(eventManager);
+    WordFrequencyManager wfm = new WordFrequencyManager(eventManager);
+    WordFrequencyController wfc = new WordFrequencyController(eventManager);
+    Output output = new Output(eventManager, range, sortOrder);
+    
+    eventManager.publish(EventType.START, dataPath);
+   }
 }
