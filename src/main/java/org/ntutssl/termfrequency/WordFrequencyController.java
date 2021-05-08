804dad7b8d85e3cc58a422240ed26e42688365f8
107590006 <t107590006@ntut.org.tw>
2021-05-08 19:35:27 +0800

    WFC, completed

--- src/main/java/org/ntutssl/termfrequency/WordFrequencyController.java
+++ src/main/java/org/ntutssl/termfrequency/WordFrequencyController.java
@@ -1,8 +1,16 @@
 package org.ntutssl.termfrequency;
 
 public class WordFrequencyController implements EventListener {
-  
-  public WordFrequencyController(EventManager eventManager) { }
+  private EventManager eventManager;
+  public WordFrequencyController(EventManager eventManager) {
+    this.eventManager=eventManager;
+    this.eventManager.subscribe(EventType.START, this);
+   }
 
-  public void onEvent(EventType eventType, String event) { }
+  public void onEvent(EventType eventType, String event) {
+    if(EventType.START==eventType) {
+      eventManager.publish(EventType.LOAD, event);
+      eventManager.publish(EventType.RUN, "");
+    }
+   }
 }
