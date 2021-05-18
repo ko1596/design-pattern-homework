cd7ea1fcbe60ee11ddeb0be2fa577469dfce390d
ko1596 <ko89022189@gmail.com>
2021-05-18 18:25:32 +0800

    title finished

--- src/main/java/org/ntutssl/document/Title.java
+++ src/main/java/org/ntutssl/document/Title.java
@@ -2,12 +2,33 @@ package org.ntutssl.document;
 
 public class Title implements Document {
 
-  public Title(String text, int size) { }
+  private String text;
+  private int size;
 
-  public String getText() { }
+  public Title(String text, int size) {
+    this.text = text;
+    this.size = size;
+   }
 
-  public int getSize() { }
+   public String getText() {
+    return this.text;
+  }
+
+  public int getSize() {
+    return this.size;
+  }
 
   @Override
-  public String toString() { }
+  public String toString() {
+    String string = "";
+    string += "Title\t\t";
+    string += "text: ";
+    string += this.text;
+    string += "\n";
+    string += "\t\tsize: ";
+    string += this.size;
+    string += "\n";
+    return string;
+
+  }
 }
