aeff67a36032216da682fa836cd2d02f42f0a0ce
ko1596 <ko89022189@gmail.com>
2021-05-18 18:56:36 +0800

    FCC finished

--- src/main/java/org/ntutssl/document/FindContentConsumer.java
+++ src/main/java/org/ntutssl/document/FindContentConsumer.java
@@ -14,5 +14,31 @@ public class FindContentConsumer implements Consumer<Document> {
     this.target = target.toLowerCase();
    }
 
-  public void accept(Document document) { }
+   public void accept(Document document){
+    if(document.getClass().equals(Title.class)) visitTitle((Title)document);
+    if(document.getClass().equals(Paragraph.class)) visitParagraph((Paragraph)document);
+    if(document.getClass().equals(Article.class)) visitArticle((Article)document);
+  }
+
+  private void visitParagraph(Paragraph paragraph) {
+    if(paragraph.getText().toLowerCase().contains(this.target) && (this.target.length() > 0 || paragraph.getText().length() == 0)) {
+      this.result.add(paragraph);
+    }
+  }
+
+  private void visitTitle(Title title) {
+    if(title.getText().toLowerCase().contains(this.target) && (this.target.length() > 0 || title.getText().length() == 0)) {
+      this.result.add(title);
+    }
+  }
+
+  private void visitArticle(Article article) {
+    if(article.getText().toLowerCase().contains(this.target) && (this.target.length() > 0 || article.getText().length() == 0)) {
+      this.result.add(article);
+    }
+
+    Iterator<Document> it = article.iterator();
+    while(it.hasNext())
+      this.accept(it.next());
+  }
 }
