f37be597ea489776380d2f23f686fcaa02f3ded2
ko1596 <ko89022189@gmail.com>
2021-05-18 18:22:01 +0800

    Doucument finished

--- src/main/java/org/ntutssl/document/Document.java
+++ src/main/java/org/ntutssl/document/Document.java
@@ -1,12 +1,20 @@
 package org.ntutssl.document;
 public interface Document {
-  public default int getLevel() { }
+  public default int getLevel() {
+    throw new DocumentException("getLevel function is not found");
+  }
   
-  public default void add(Document document) { }
+  public default void add(Document document) {
+    throw new DocumentException("add function is not found");
+  }
   
-  public default Iterator<Document> iterator() { }
+  public default Iterator<Document> iterator() {
+    throw new NoSuchElementException("NoSuchElementException");
+  }
   
-  public default int getSize() { }
+  public default int getSize() {
+    throw new DocumentException("getSize function is not found");
+  }
   
   public String getText();
 
