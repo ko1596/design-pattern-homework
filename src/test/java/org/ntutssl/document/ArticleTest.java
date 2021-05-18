d1ce3a1df7f22d45f804e6b1d630ac886f641536
ko1596 <ko89022189@gmail.com>
2021-05-18 21:53:48 +0800

    null it

--- src/test/java/org/ntutssl/document/ArticleTest.java
+++ src/test/java/org/ntutssl/document/ArticleTest.java
@@ -3,22 +3,70 @@ package org.ntutssl.document;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import java.util.NoSuchElementException;
 import java.util.Iterator;
 
 public class ArticleTest {
     @Test
-    public void test_add_high_level_artical(){
-        Article article = new Article("aa", 1);
-        Article article2 = new Article("bb", 2);
-        article.add(article2);
+    public void test_add_an_article_with_higher_level_into_article(){
+        Document article = new Article("abc",3);
+        Document article_high = new Article("test",4);
+        article.add(article_high);
         Iterator<Document> it = article.iterator();
-        assertEquals(it.next(), article2);
+        while(it.hasNext()) assertEquals(article_high, it.next()); 
     }
 
     @Test(expected = DocumentException.class)
-    public void test_add_low_level_artical_should_exception(){
-        Article article = new Article("aa", 1);
-        article.add(new Article("bb", 0));
+    public void test_add_an_article_with_lower_level_into_article(){
+        Document article = new Article("abc",3);
+        Document article_low = new Article("abc",2);
+        article.add(article_low);
+    }
+
+    @Test(expected = DocumentException.class)
+    public void test_add_an_article_with_same_level_into_article(){
+        Document article = new Article("abc",3);
+        Document article_low = new Article("abc",3);
+        article.add(article_low);
+    }
+
+    @Test
+    public void test_get_inner_document_text_after_add_some_documents(){
+        Document article = new Article("main",3);
+        Document article_test = new Article("test",4);
+        article.add(article_test);
+        Iterator<Document> it = article.iterator();
+        while(it.hasNext()) assertEquals("test", it.next().getText()); 
+    }
+
+    @Test
+    public void test_get_level_of_article(){
+        Document article = new Article("main",3);
+        assertEquals(3, article.getLevel()); 
+    }
+
+    @Test(expected = DocumentException.class)
+    public void test_get_size_should_throw_exception(){
+        Document article = new Article("abc",3);
+        article.getSize();
+    }
+
+    @Test
+    public void test_get_text_after_add_another_document(){
+        Document article = new Article("main",3);
+        Document article_test = new Article("test",4);
+        article.add(article_test);
+        assertEquals("main", article.getText());
+    }
+
+    @Test
+    public void test_get_text_of_article(){
+        Document article = new Article("main",3);
+        assertEquals("main", article.getText());
+    }
+
+    @Test
+    public void test_to_string(){
+        Document article = new Article("main",3);
+        assertEquals("Article\t\ttopic: main\n\t\tlevel: 3\n", article.toString());
     }
  }
--- src/test/java/org/ntutssl/document/NullIteratorTest.java
+++ src/test/java/org/ntutssl/document/NullIteratorTest.java
@@ -1,3 +1,20 @@
 package org.ntutssl.document;
 
-public class NullIteratorTest { }
+import static org.junit.Assert.assertFalse;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+public class NullIteratorTest {
+
+    @Test
+    public void hasNextIswork(){
+        assertFalse(new NullIterator().hasNext());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void emptyIteratorUseNextShouldThrowException(){
+        new NullIterator().next();
+    }
+ }
