6e6a5d2852b65904d21628800966be2d973ca193
ko1596 <ko89022189@gmail.com>
2021-05-18 18:48:31 +0800

    temp

--- src/main/java/org/ntutssl/document/FindContentConsumer.java
+++ src/main/java/org/ntutssl/document/FindContentConsumer.java
@@ -5,7 +5,14 @@ public class FindContentConsumer implements Consumer<Document> {
   /**
    * @param result you should add the document which contains `target` into here.
    */
-  public FindContentConsumer(List<Document> result, String target) { }
+
+  private List<Document> result;
+  private String target;
+
+  public FindContentConsumer(List<Document> result, String target) {
+    this.result = result;
+    this.target = target.toLowerCase();
+   }
 
   public void accept(Document document) { }
 }
