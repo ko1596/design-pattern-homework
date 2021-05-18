8c84c3e6e137610c49ad0da0b03b35014690db0a
ko1596 <ko89022189@gmail.com>
2021-05-18 21:01:31 +0800

    Editor finished

--- src/main/java/org/ntutssl/document/Editor.java
+++ src/main/java/org/ntutssl/document/Editor.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.BufferedOutputStream;
 import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
 import com.google.gson.*;
-import java.io.File;
 
 public class Editor {
 
@@ -22,12 +22,36 @@ public class Editor {
    }
 
   public void importDocumentFromJsonFile(String filePath) {
-    
+    try(JsonReader jReader = new JsonReader(new InputStreamReader(new FileInputStream(filePath),"UTF-8")))
+    {
+        JsonArray jArray = JsonParser.parseReader(jReader).getAsJsonArray();
+        for(JsonElement jElement : jArray){
+            DocumentParser documentParser = new DocumentParser();
+            this.add(documentParser.parse(jElement.getAsJsonObject()));
+        }
+    }catch(Exception e){
+        e.printStackTrace();
+    }
    }
 
-  public void exportDocumentAsHtmlFile(String outputPath) { }
+  public void exportDocumentAsHtmlFile(String outputPath) {
+    try{
+      List<String> results = new ArrayList<>();
+      HtmlOutputConsumer hoc = new HtmlOutputConsumer(results);
+      documentList.forEach(hoc);
+      Path path = Paths.get(outputPath);
+      Files.write(path, results, StandardCharsets.UTF_8);
+    }catch(IOException e){
+      throw new DocumentException("write fail.");
+    }
+  }
 
-  public void findContent(String target) { }
+  public void findContent(String target) {
+    List<Document> results = new ArrayList<>();
+    FindContentConsumer fcc = new FindContentConsumer(results,target);
+    documentList.forEach(fcc);
+    results.forEach(System.out::print);
+  }
 
   public void add(Document document) {
     documentList.add(document);
--- src/main/java/org/ntutssl/document/HtmlOutputConsumer.java
+++ src/main/java/org/ntutssl/document/HtmlOutputConsumer.java
@@ -6,6 +6,17 @@ import java.util.Iterator;
 
 public class HtmlOutputConsumer implements Consumer<Document> {
 
+  /**
+   * @param result you should add the result into here.
+   * 
+   * example:
+   * this.result.get(0) = "<article topic='topic1'>\n";
+   * this.result.get(1) = "  <h1>title1</h1>\n";
+   * this.result.get(2) = "  <p>paragraph1</p>\n";
+   * this.result.get(3) = "  <h2>title2</h2>\n";
+   * this.result.get(4) = "</article>\n";
+   */
+
   private List<String> result;
   private String string;
 
