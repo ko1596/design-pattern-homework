9b1f933df0392e639fd2b69f8437c7098dc32b49
ko1596 <ko89022189@gmail.com>
2021-06-01 16:00:25 +0800

    GBT

--- src/test/java/org/ntutssl/shop/GoodsBuilderTest.java
+++ src/test/java/org/ntutssl/shop/GoodsBuilderTest.java
@@ -1,3 +1,53 @@
 package org.ntutssl.shop;
 
-public class GoodsBuilderTest { }
+import static org.junit.Assert.assertEquals;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+
+
+public class GoodsBuilderTest { 
+    @Test
+    public void test_build_merchandise() {
+        GoodsBuilder goodsBuilder = new GoodsBuilder();
+        goodsBuilder.buildMerchandise(1, "merchandise", "test", 1.0);
+        assertEquals(1, goodsBuilder.getResult().id());
+        assertEquals("merchandise", goodsBuilder.getResult().name());
+        assertEquals("test", goodsBuilder.getResult().description());
+        assertEquals(1, goodsBuilder.getResult().price(), 1);
+        
+    }
+
+    @Test
+    public void test_build_collection() {
+        GoodsBuilder goodsBuilder = new GoodsBuilder();
+        goodsBuilder.startBuildCollection(1, "collection", "test");
+        goodsBuilder.endBuildCollection();
+        assertEquals(1, goodsBuilder.getResult().id());
+        assertEquals("collection", goodsBuilder.getResult().name());
+        assertEquals("test", goodsBuilder.getResult().description());
+    }
+
+    @Test
+    public void test_build_collection_contains_merchandises() {
+        GoodsBuilder goodsBuilder = new GoodsBuilder();
+        goodsBuilder.startBuildCollection(1, "collection", "test");
+        goodsBuilder.buildMerchandise(1, "merchandise", "test", 1.0);
+        goodsBuilder.endBuildCollection();
+
+        Iterator<Goods> ite = goodsBuilder.getResult().iterator();
+
+        assertEquals(1, goodsBuilder.getResult().id());
+        assertEquals("collection", goodsBuilder.getResult().name());
+        assertEquals("test", goodsBuilder.getResult().description());
+
+        Goods goods = ite.next();
+
+        assertEquals(1, goods.id());
+        assertEquals("merchandise", goods.name());
+        assertEquals("test", goods.description());
+        assertEquals(1, goods.price(), 1);
+    }
+
+}
