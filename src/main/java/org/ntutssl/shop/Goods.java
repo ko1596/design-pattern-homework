4da2449200f7b111f646fc1c93112e0cb849a9f4
ko1596 <ko89022189@gmail.com>
2021-06-01 13:22:48 +0800

    GOOD

--- src/main/java/org/ntutssl/shop/Goods.java
+++ src/main/java/org/ntutssl/shop/Goods.java
@@ -1,11 +1,18 @@
 package org.ntutssl.shop;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public interface Goods {
   public int id();
   public double price();
   public String name();
   public String description();
 
-  public default void add(Goods goods) { }
-  public default Iterator<Goods> iterator() { }
+  public default void add(Goods goods) {
+    throw new ShopException("add function is not found");
+   }
+  public default Iterator<Goods> iterator() {
+    throw new NoSuchElementException("NoSuchElementException");
+   }
 }
