6258ef5ca69d1f7bc5090aa9cd9e25e6d488419c
ko1596 <ko89022189@gmail.com>
2021-06-01 16:10:01 +0800

    GBT

--- src/test/java/org/ntutssl/shop/GoodsBuilderTest.java
+++ src/test/java/org/ntutssl/shop/GoodsBuilderTest.java
@@ -16,25 +16,6 @@ public class GoodsBuilderTest {
         assertEquals("merchandise", goodsBuilder.getResult().name());
         assertEquals("test", goodsBuilder.getResult().description());
         assertEquals(1, goodsBuilder.getResult().price(), 1);
-        
-    }
-
-
-    @Test
-    public void test_build_collection_contains_merchandises() {
-        GoodsBuilder goodsBuilder = new GoodsBuilder();
-        goodsBuilder.buildMerchandise(1, "merchandise", "test", 1.0);
-        goodsBuilder.startBuildCollection(1, "collection", "test");
-        goodsBuilder.endBuildCollection();
-        Iterator<Goods> ite = goodsBuilder.getResult().iterator();
-        assertEquals(1, goodsBuilder.getResult().id());
-        assertEquals("collection", goodsBuilder.getResult().name());
-        assertEquals("test", goodsBuilder.getResult().description());
-        Goods goods = ite.next();
-        assertEquals(1, goods.id());
-        assertEquals("merchandise", goods.name());
-        assertEquals("test", goods.description());
-        assertEquals(1, goods.price(), 1);
     }
 
     @Test
