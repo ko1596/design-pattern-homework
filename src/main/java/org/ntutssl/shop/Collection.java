2b8ff1573c82b6418bf7d89a0f70eb94fb6c02c8
ko1596 <ko89022189@gmail.com>
2021-06-01 13:32:39 +0800

    colllition

--- src/main/java/org/ntutssl/shop/Collection.java
+++ src/main/java/org/ntutssl/shop/Collection.java
@@ -1,5 +1,9 @@
 package org.ntutssl.shop;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Collection implements Goods {
   
   /**
@@ -8,5 +12,52 @@ public class Collection implements Goods {
    * @param name
    * @param desc
    */
-  public Collection(int id, String name, String desc) { }
+  private int ids;
+  private String names;
+  private double prices;
+  private String descriptions;
+  private List<Goods> collitionLists;
+
+  public Collection(int id, String name, String desc) {
+    this.ids = id;
+    this.names = name;
+    this.descriptions = desc;
+    this.collitionLists = new ArrayList<>();
+   }
+
+  @Override
+  public int id() {
+    // TODO Auto-generated method stub
+    return ids;
+  }
+
+  @Override
+  public double price() {
+    // TODO Auto-generated method stub
+    return prices;
+  }
+
+  @Override
+  public String name() {
+    // TODO Auto-generated method stub
+    return names;
+  }
+
+  @Override
+  public String description() {
+    // TODO Auto-generated method stub
+    return descriptions;
+  }
+
+  @Override
+  public void add(Goods goods) {
+      // TODO Auto-generated method stub
+      Goods.super.add(goods);
+  }
+
+  @Override
+  public Iterator<Goods> iterator() {
+      // TODO Auto-generated method stub
+      return this.collitionLists.iterator();
+  }
 }
