0555a2465fc894f33d2e046cff2e95d92fcfe942
ko1596 <ko89022189@gmail.com>
2021-06-15 17:42:52 +0800

    IH add

---
+++ src/main/java/org/ntutssl/shop/InstructionHandler.java
@@ -0,0 +1,20 @@
+package org.ntutssl.shop;
+
+public class InstructionHandler {
+
+  public InstructionHandler() { }
+
+  public void run() { }
+
+  private void printInstructions() { }
+
+  private void handleInstruction(String instruction) { }
+
+  private void instructionListShop() { }
+
+  private void instructionImportShoppingList() { }
+
+  private void instructionListShoppingCart() { }
+
+  private void instructionPay() { }
+}
