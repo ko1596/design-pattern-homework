d58474fa1757c837a71b5cac1699a92a0051801f
ko1596 <ko89022189@gmail.com>
2021-06-15 20:16:36 +0800

    EventTest add

--- src/test/java/org/ntutssl/shop/EventTest.java
+++ src/test/java/org/ntutssl/shop/EventTest.java
@@ -1,3 +1,22 @@
 package org.ntutssl.shop;
 
-public class EventTest { }
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class EventTest {
+    @Test
+    public void test_good(){
+        Collection collection = new Collection(1,"collection","good");
+        GoodsEvent event = new GoodsEvent(EventType.ADD_TO_CART, collection, 1);
+        assertEquals(event.type(),EventType.ADD_TO_CART);
+        assertEquals(event.data(),collection);
+        assertEquals(event.count(),1);
+    }
+    @Test 
+    public void test_string(){
+        StringEvent event = new StringEvent(EventType.ADD_TO_CART, "string");
+        assertEquals(event.type(),EventType.ADD_TO_CART);
+        assertEquals(event.data(),"string");
+    }
+ }
